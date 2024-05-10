5df83f43a405a3d2627cf8c4d5b9ef9c182b15fe
barunstha007 <barun.shrestha@classifiedsventures.com>
2024-05-10 16:57:04 +0545

    necessary modules added

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes/TodoRoute')
+const Cors = require('cors')
+require('dotenv').config()
 
-require( 'dotenv' ).config()
-
+// App config
 const app = express()
+
+const PORT = process.env.PORT || 5000
+
+//Convert to json
 app.use(express.json())
+app.use(Cors())
 app.use(
 	express.urlencoded({
 		extended: true,
 	})
 )
-const PORT = process.env.PORT || 5000
 
 //Mongodb Connection
 mongoose
 	.connect(process.env.MONGODB_URL)
 	.then(() => console.log(`Connected To MongoDB`))
-	.catch(() => console.log(err))
-
+	.catch( () => console.log( err ) )
+	
+//API end point
 app.use(routes)
 
 app.listen(PORT, () => console.log(`Listening on port:${PORT}`))
